2012e36ae4fcdd99af57df10aa3d52722be741e2
JOSE MIGUEL CHICAS MARTINEZ <ISNP052524@ugb.edu.sv>
2024-11-03 20:02:18 -0600

    Agregar archivos de proyecto.

---
+++ ACTIVIDADBLOQUE_3/ACTIVIDADBLOQUE_3/Models/MyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace ACTIVIDADBLOQUE_3.Models
+{
+
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+        public DbSet<Pelicula> peliculas { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Pelicula>().HasKey(Pelicula => Pelicula.idPelicula);
+        }
+    }
+
+}
---
+++ ACTIVIDADBLOQUE_3/ACTIVIDADBLOQUE_3/Models/Pelicula.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ACTIVIDADBLOQUE_3.Models 
+{ 
+    public class Pelicula
+    {
+    [Key]
+    public int idPelicula { get; set; }
+    public string titulo { get; set; }
+    public string autor { get; set; }
+    public string sinopsis { get; set; }
+    public int duracion { get; set; }
+    public string clasificacion { get; set; }
+    }
+
+    }
+
